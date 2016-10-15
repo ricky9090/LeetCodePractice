497165a0d55ca97abc8b0737d7102923c6e54fad
k10ndike <unreal_star13@126.com>
2016-10-15 16:42:54 +0800

    On branch master  Changes to be committed: 	new file:   src/test/leetcode1/Solution.java 	new file:   src/test/leetcode2/ListNode.java 	new file:   src/test/leetcode2/Solution.java

---
+++ src/test/leetcode1/Solution.java
@@ -0,0 +1,35 @@
+package test.leetcode1;
+
+import java.util.HashMap;
+
+public class Solution {
+	public int[] twoSum(int[] nums, int target) {
+		int length = nums.length;
+		// <num, index>
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		
+		for (int i = 0; i < length; i++) {
+			map.put(nums[i], i);
+		}
+		
+		for (int n = 0; n < length; n++) {
+			int a = nums[n];
+			int b = target - a;
+			if ((map.get(b) != null) && (map.get(b) != n)) {
+				int[] result = {n, map.get(b)};
+				return result;
+			}
+		}
+        
+		return null;
+    }
+	
+	public static void main(String[] args) {
+		int[] a = {2, 7, 5, 11};
+		Solution test = new Solution();
+		int[] result = test.twoSum(a, 9);
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+}
