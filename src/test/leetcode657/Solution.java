d36ee3ae208255419bc212a7aa70d51cfd02b351
ucx9002 <ucx9002@github.com>
2017-08-27 01:16:10 +0800

    add solution657

---
+++ src/test/leetcode657/Solution.java
@@ -0,0 +1,42 @@
+package test.leetcode657;
+
+public class Solution {
+    public boolean judgeCircle(String moves) {
+        if (moves == null) {
+            return true;
+        }
+        if ("".equals(moves)) {
+            return true;
+        }
+        
+        int[] result = {0,0};
+        char[] movements = moves.toCharArray();
+        for (int i = 0; i < moves.length(); i++) {
+            switch (movements[i]) {
+            case 'U':
+                result[0] += 1;
+                break;
+            case 'D':
+                result[0] -= 1;
+                break;
+            case 'L':
+                result[1] -= 1;
+                break;
+            case 'R':
+                result[1] += 1;
+                break;
+            default:
+                break;
+            }
+        }
+        if (result[0] == 0 && result[1] == 0) {
+            return true;
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        Solution test = new Solution();
+        System.out.println(test.judgeCircle("ASAS"));
+    }
+}
