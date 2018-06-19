1ee69598e2de4bc462b5fd546d6c36e938dca1c7
ucx9002 <unreal_star13@126.com>
2018-06-19 16:13:34 +0800

    add solution 141

---
+++ src/main/java/study/leetcode141/Solution.java
@@ -0,0 +1,43 @@
+package study.leetcode141;
+
+/**
+ * Given a linked list, determine if it has a cycle in it.
+ *
+ * @see <a href="https://leetcode.com/problems/linked-list-cycle/description/">
+ *     Linked List Cycle - LeetCode</a>
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode start = head;
+        ListNode tail = head;
+
+        if (start.next == null) {
+            return false;
+        }
+        if (start.next == start) {
+            return true;
+        }
+
+        tail = start.next.next;
+        while(start != tail) {
+            if (start == null || tail== null || tail.next == null) {
+                return false;
+            }
+            start = start.next;
+            tail = tail.next.next;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = null;
+
+        Solution solution = new Solution();
+        System.out.println(solution.hasCycle(head));
+    }
+}
