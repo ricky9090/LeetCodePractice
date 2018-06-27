30a21b68720b4aa7d9eb1f548df959e767e3784b
ucx9002 <unreal_star13@126.com>
2018-06-27 22:40:18 +0800

    add javadoc for solution 70

--- src/main/java/study/leetcode70/Solution.java
+++ src/main/java/study/leetcode70/Solution.java
@@ -1,5 +1,15 @@
 package study.leetcode70;
 
+/**
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ * <p>
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ * <p>
+ * Note: Given n will be a positive integer.
+ *
+ * @see <a href="https://leetcode.com/problems/climbing-stairs/description/">
+ * Climbing Stairs - LeetCode</a>
+ */
 public class Solution {
     public int climbStairs(int n) {
         if (n <= 0) {
