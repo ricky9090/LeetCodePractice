372a9eb308972fca4393e0eff7b3ca62634ed09a
ucx9002 <unreal_star13@126.com>
2019-04-10 22:12:21 +0800

    add solution 155

---
+++ src/test/java/study/leetcode/p200/no155/SolutionTest.java
@@ -0,0 +1,51 @@
+package study.leetcode.p200.no155;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class SolutionTest {
+    MinStack stack;
+
+    @Before
+    public void setUp() {
+        stack = new MinStack();
+    }
+
+    @After
+    public void tearDown() {
+        stack = null;
+    }
+
+    @Test
+    public void test1() {
+        stack.push(-2);
+        stack.push(0);
+        stack.push(-3);
+        assertEquals(-3, stack.getMin());
+        stack.pop();
+        assertEquals(-2, stack.getMin());
+        assertEquals(0, stack.top());
+
+    }
+
+    @Test
+    public void test2() {
+        stack.push(2);
+        stack.push(0);
+        stack.push(3);
+        stack.push(0);
+        assertEquals(0, stack.getMin());
+        stack.pop();
+        assertEquals(0, stack.getMin());
+        stack.pop();
+        assertEquals(0, stack.getMin());
+        stack.pop();
+        assertEquals(2, stack.getMin());
+    }
+}
