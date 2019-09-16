b57ae544bb4ab9b6d872ef2b726d2ec7a4f189ce
ucx9002 <unreal_star13@126.com>
2019-09-16 14:32:42 +0800

    add no191

---
+++ src/main/java/study/leetcode/section150/no191/Solution.java
@@ -0,0 +1,24 @@
+package study.leetcode.section150.no191;
+
+/**
+ * Write a function that takes an unsigned integer and return
+ * the number of '1' bits it has (also known as the Hamming weight).
+ *
+ * @see <a href="https://leetcode.com/problems/number-of-1-bits/">
+ * Number of 1 Bits</a>
+ */
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int target = n;
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            int tmp = target & 1;
+            if (tmp == 1) {
+                count++;
+            }
+            target = target >>> 1;
+        }
+        return count;
+    }
+}
