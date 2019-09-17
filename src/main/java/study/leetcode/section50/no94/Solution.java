49fc6a5bff2c04dfc6baef6c136afe45035b15fe
ucx9002 <unreal_star13@126.com>
2019-09-17 16:23:36 +0800

    add no94

---
+++ src/main/java/study/leetcode/section50/no94/Solution.java
@@ -0,0 +1,49 @@
+package study.leetcode.section50.no94;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * <p>
+ * <b>Follow up:</b> Recursive solution is trivial, could you do it iteratively?
+ *
+ * @see <a href="https://leetcode.com/problems/binary-tree-inorder-traversal/">
+ * Binary Tree Inorder Traversal</a>
+ */
+public class Solution {
+    // recursive solution
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        if (root.left == null && root.right == null) {
+            result.add(root.val);
+            return result;
+        }
+
+        List<Integer> left = inorderTraversal(root.left);
+        List<Integer> right = inorderTraversal(root.right);
+
+        if (left != null) {
+            result.addAll(left);
+        }
+        result.add(root.val);
+        if (right != null) {
+            result.addAll(right);
+        }
+
+        return result;
+    }
+
+    protected static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
---
+++ src/main/java/study/leetcode/section50/no94/SolutionB.java
@@ -0,0 +1,11 @@
+package study.leetcode.section50.no94;
+
+import java.util.List;
+
+public class SolutionB {
+
+    public List<Integer> inorderTraversal(Solution.TreeNode root) {
+        // TODO iteratively solution
+        return null;
+    }
+}
