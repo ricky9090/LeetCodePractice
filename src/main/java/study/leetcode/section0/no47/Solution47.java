4ff6802d46374f44b7da9039ab08cb8ae08ffae9
ucx9002 <unreal_star13@126.com>
2019-09-25 23:26:43 +0800

    add no47

--- src/main/java/study/leetcode/section0/no47/Solution47.java
+++ src/main/java/study/leetcode/section0/no47/Solution47.java
@@ -1,32 +1,30 @@
-package study.leetcode.section0.no46;
+package study.leetcode.section0.no47;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
- * <h2>Permutations</h2>
- * Given a collection of distinct integers, return all possible permutations.
+ * <h2>Permutations II</h2>
+ * Given a collection of numbers that might contain duplicates, return all possible unique permutations.
  * <p>
  * <b>Example:</b>
  * <pre><code>
- * Input: [1,2,3]
+ * Input: [1,1,2]
  * Output:
  * [
- *   [1,2,3],
- *   [1,3,2],
- *   [2,1,3],
- *   [2,3,1],
- *   [3,1,2],
- *   [3,2,1]
+ *   [1,1,2],
+ *   [1,2,1],
+ *   [2,1,1]
  * ]
  * </code></pre>
  *
- * @see <a href="https://leetcode-cn.com/problems/permutations">
- * Permutations</a>
+ * @see <a href="https://leetcode-cn.com/problems/permutations-ii">
+ * Permutations II</a>
  */
-public class Solution46 {
-    public List<List<Integer>> permute(int[] nums) {
+public class Solution47 {
+    public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         if (nums == null) {
             return result;
@@ -36,6 +34,7 @@ public class Solution46 {
         for (int i = 0; i < nums.length; i++) {
             remain.add(nums[i]);
         }
+        Collections.sort(remain);
 
         generate(gen, remain, nums.length, result);
         return result;
@@ -51,9 +50,13 @@ public class Solution46 {
         for (int i = 0; i < remainSize; i++) {
             List<Integer> nextRemain = new LinkedList<>(remain);
             List<Integer> nextGen = new ArrayList<>(genList);
+            if (i > 0 && (nextRemain.get(i - 1).equals(nextRemain.get(i)))) {
+                continue;
+            }
             nextGen.add(nextRemain.get(i));
             nextRemain.remove(i);
             generate(nextGen, nextRemain, remainSize - 1, result);
+
         }
     }
 }
